3beb05515c7f79291d0351350878a858cb923584
Aquavital <160146371+Aquavital@users.noreply.github.com>
2024-04-16 11:59:56 +0200

    Update index.php

--- index.php
+++ index.php
@@ -68,40 +68,7 @@
 
   
     
-    <script>
-    document.getElementById("keres").addEventListener("click", function() {//A kattintásra az évfolyam és az osztály   értéke ellesz küldve az adatbázisnak .
-  var evfolyam = document.getElementById("evfolyam").value;
-  var osztaly = document.getElementById("osztaly").value;
-
-  
-  var xhr = new XMLHttpRequest();//xml objektum
-  xhr.open("POST", "rest.php", true);//Post kérést küld a rest.php-nak
-  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");//Kódolt adatok beállítása
-  xhr.onreadystatechange = function() {//Függvény létrehozása
-    if (xhr.readyState === 4 && xhr.status === 200) {//4-re Vált a kérés tehát  befejezett és 200 azért , hogy a kérés sikeres volt 
-      var nevsor = JSON.parse(xhr.responseText);//Json válasszá 
-      nevsor.sort(); // ABC sorrendbe rendezés
-
-      
-      var nevHalmaz = new Set(); //Az ismétlődések kiszűrése miatt kell
-      var nevsorHTML = "<ol>";//Számozott lista
-
-      nevsor.forEach(function(nev) {
-        if (!nevHalmaz.has(nev)) { // Ellenőrizzük, hogy a név szerepel-e már a halmazban ha igen akkor kihagyjuk
-          nevHalmaz.add(nev); // Ha nem, hozzáadjuk a halmazhoz
-          nevsorHTML += "<li>" + nev + "</li>"; // Név hozzáadása a listához
-        }
-      });
-
-      nevsorHTML += "</ol>";
-      document.getElementById("eredmeny").innerHTML = nevsorHTML;//Kiírjuk az eredményt az eredmény listába 
-    }
-  };
-  xhr.send("evfolyam=" + evfolyam + "&osztaly=" + osztaly);//Post kérés küldése a servernek 
-});
-
-</script>
-
+   
 
 </body>
 </html>
